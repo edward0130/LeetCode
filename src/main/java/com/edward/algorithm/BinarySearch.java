0df04d21f6ff8fb03e2e6f128fd752e15708816f
edward0130 <edward30@163.com>
2022-05-18 14:28:24 +0800

    created project

---
+++ src/main/java/com/edward/algorithm/BinarySearch.java
@@ -0,0 +1,14 @@
+package com.edward.algorithm;
+
+/**
+ *
+ * 二分查找
+ *
+ * 相关题目：704
+ *
+ */
+public class BinarySearch {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/com/edward/problems/P704.java
@@ -0,0 +1,14 @@
+package com.edward.problems;
+
+public class P704 {
+    public int search(int[] nums, int target) {
+
+
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
